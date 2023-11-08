02a343ef3c03570be0f63fa240417475e7896e13
tuck-ab <tuckabh@gmail.com>
2023-11-08 17:39:52 +0000

    Refactor main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,31 +3,23 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let context_result = parser::get_file_context(&(args[1]));
+    let context = parser::get_file_context(&(args[1]))
+                                        .expect(&format!("Could not open file: {:?}", &(args[1])));
 
-    let context;
-    match context_result {
-        None => panic!("Could not open file: {:?}", &(args[1])),
-        Some(c) => context = c
-    }
-
+    
     let parse_result = parser::gen_ast(context);
 
     parser::close_file_from_context(context);
 
-    let ast;
-    match parse_result {
+    let ast = match parse_result {
         Err(e) => panic!("PARSING FAILED :(: {:?}", e),
-        Ok(a) => ast = a
-    }
+        Ok(a) => a
+    };
 
-    let result;
-    match interpreter::interpret(ast) {
+    let result = match interpreter::interpret(ast) {
         Err(e) => panic!("Interpreting Failed :( : {:?}", e),
-        Ok(r) => result = r
-    }
-
-
+        Ok(r) => r
+    };
 
     for item in result.iter() {
         println!("{}", item);
